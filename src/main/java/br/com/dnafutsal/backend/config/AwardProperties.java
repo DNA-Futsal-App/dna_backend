33f760471b827d46a8e7b5f5318666b0075d68aa
Alexandre <alex.silva250@hotmail.com>
2026-09-17 01:45:56 -0300

    fix: atualização do AwardProperties

--- src/main/java/br/com/dnafutsal/backend/config/AwardProperties.java
+++ src/main/java/br/com/dnafutsal/backend/config/AwardProperties.java
@@ -2,6 +2,7 @@ package br.com.dnafutsal.backend.config;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
@@ -12,6 +13,15 @@ public record AwardProperties(
         @NotNull Duration coachInviteTtl,
         @NotNull Duration coachInviteReservationTtl
 ) {
+
+    @ConstructorBinding
+    public AwardProperties(
+            Duration coachInviteTtl,
+            Duration coachInviteReservationTtl
+    ) {
+        this.coachInviteTtl = coachInviteTtl;
+        this.coachInviteReservationTtl = coachInviteReservationTtl;
+    }
 
     public AwardProperties(
             Duration coachInviteTtl
