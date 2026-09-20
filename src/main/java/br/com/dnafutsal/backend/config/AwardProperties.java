60d58a0a73aa68e4d2d051349fa62adb04924618
Alexandre <alex.silva250@hotmail.com>
2026-09-20 18:23:34 -0300

    Revert "fix: atualização do AwardProperties"
    This reverts commit 33f760471b827d46a8e7b5f5318666b0075d68aa.

--- src/main/java/br/com/dnafutsal/backend/config/AwardProperties.java
+++ src/main/java/br/com/dnafutsal/backend/config/AwardProperties.java
@@ -2,7 +2,6 @@ package br.com.dnafutsal.backend.config;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
@@ -13,15 +12,6 @@ public record AwardProperties(
         @NotNull Duration coachInviteTtl,
         @NotNull Duration coachInviteReservationTtl
 ) {
-
-    @ConstructorBinding
-    public AwardProperties(
-            Duration coachInviteTtl,
-            Duration coachInviteReservationTtl
-    ) {
-        this.coachInviteTtl = coachInviteTtl;
-        this.coachInviteReservationTtl = coachInviteReservationTtl;
-    }
 
     public AwardProperties(
             Duration coachInviteTtl
